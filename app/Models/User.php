fb84c208432cc38ebd915c96b541ca34fdcbb92a
wildanhawari <wildanhawari702@gmail.com>
2025-01-17 23:39:47 +0700

    Production: fix access panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, 'resepin.up.railway.app') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@resepin.com') && $this->hasVerifiedEmail();
     }
 
 }
