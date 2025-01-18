acb13719c4b0fd695ddbb7319a76fb9c8fe21282
wildanhawari <wildanhawari702@gmail.com>
2025-01-18 08:39:38 +0700

    Production: bug

--- build-app.sh
+++ build-app.sh
@@ -1,1 +1,1 @@
-php artisan storage:link && php artisan config:cache && php artisan config:clear && php artisan filament:optimize
+php artisan storage:link && php artisan config:cache && php artisan config:clear && php artisan filament:optimize && php artisan config:cache && php artisan route:cache && php artisan view:cache
